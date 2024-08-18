1a77d485f70592bef2de97bb6a708453d61a5f1a
parth1010444 <59702388+parth1010444@users.noreply.github.com>
2024-08-18 09:18:12 +0000

    variables and Data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = "12345"
+let accountEmail = "xyz@gmail.com";
+var accountPassword = "12345";
+
+// accountId = "12";
+
+console.log(accountId)
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,1 @@
+console.table([typeof null,typeof undefined]);
